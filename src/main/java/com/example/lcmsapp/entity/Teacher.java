5b3d0fd0caeeb7ca2f20fdb06746af03f9dad4c8
AzamatJurayev <aaab102600@gmail.com>
2022-07-02 18:16:31 +0300

    Teacher CRUD

--- src/main/java/com/example/lcmsapp/entity/Teacher.java
+++ src/main/java/com/example/lcmsapp/entity/Teacher.java
@@ -4,8 +4,10 @@ import com.example.lcmsapp.entity.enums.PositionType;
 import com.example.lcmsapp.entity.enums.StudentStatus;
 import com.example.lcmsapp.entity.template.AbsEntity;
 import lombok.*;
+import org.hibernate.type.TimestampType;
 
 import javax.persistence.*;
+import java.time.LocalDate;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -18,13 +20,26 @@ public class Teacher extends AbsEntity {
 
     private String fullName, phone;
 
-    private Double balance;
+    private Double balance = 0.0;
 
-    private boolean active;
+    private boolean active = false;
 
     @ManyToOne
     private Course course;
 
     @Enumerated
     private PositionType positionType = PositionType.MENTOR;
+
+    private Character male;
+
+    @Column(length = 9)
+    private String passportNo;
+
+    private String address;
+
+    private String passportGivenBy;
+
+    private LocalDate passportDateOfIssue;
+
+    private LocalDate dateOfBirth;
 }
