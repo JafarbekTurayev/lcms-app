5a4a6aa9da96b5d85dad6688bb694153dca990bc
JafarbekTurayev <jafarbek1997@gmail.com>
2022-07-05 19:02:19 +0500

    form based

--- src/main/java/com/example/lcmsapp/util/Pagination.java
+++ src/main/java/com/example/lcmsapp/util/Pagination.java
@@ -1,4 +1,27 @@
 package com.example.lcmsapp.util;
 
+import java.awt.print.PageFormat;
+import java.awt.print.Pageable;
+import java.awt.print.Printable;
+
 public class Pagination {
+//    public static void main(String[] args) {
+//        Pageable pageable = new Pageable() {
+//            //interfaceni realizatsiya qilayotgan anonim class
+//            @Override
+//            public int getNumberOfPages() {
+//                return 0;
+//            }
+//
+//            @Override
+//            public PageFormat getPageFormat(int pageIndex) throws IndexOutOfBoundsException {
+//                return null;
+//            }
+//
+//            @Override
+//            public Printable getPrintable(int pageIndex) throws IndexOutOfBoundsException {
+//                return null;
+//            }
+//        }
+//    }
 }
