68c415ecd5f3e12a7d9e6bc0e62b4ec99e11bfb6
AzamatJurayev <aaab102600@gmail.com>
2022-07-12 21:20:36 +0300

    SECURITY gatov

--- src/main/java/com/example/lcmsapp/config/SecurityConfig.java
+++ src/main/java/com/example/lcmsapp/config/SecurityConfig.java
@@ -1,4 +1,4 @@
-//package com.example.lcmsapp.config;
+ //package com.example.lcmsapp.config;
 //
 //import org.springframework.context.annotation.Bean;
 //import org.springframework.context.annotation.Configuration;
